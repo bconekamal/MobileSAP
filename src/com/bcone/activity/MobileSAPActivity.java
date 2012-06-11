4f982d0773164c468442494fad3d55ff8a8e2bf7
Harshal <harsh_g619@yahoo.com>
2012-06-11 13:27:42 +0530

    test1
    test1

--- src/com/bcone/activity/MobileSAPActivity.java
+++ src/com/bcone/activity/MobileSAPActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MobileSAPActivity extends Activity {
-	/** Called when the activity is first created.harsh */
+	/** Called when the activity is first created.harshal */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
